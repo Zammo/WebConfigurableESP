90d3f2b5c6e855be1a84395b740d55d039a58f9a
Zammo <JoeBoy879@gmail.com>
2015-06-01 15:10:26 +0200

    Update init.lua

--- code/init.lua
+++ code/init.lua
@@ -10,12 +10,12 @@ end
 gpio.trig(pin, "up",Connect)
 
 print(tmr.now())
--- ############################# Temp file
-file.open("settings.lua", "r")
-if (file.open("settings.lua", "r") == nil) then
-	print("No file")
-	dofile("GetNetwork.lua")
+file.open("info.lua", "r")
+if (file.open("info.lua", "r") == nil) then
+print("No file")
+dofile("GetNetwork.lua")
 end
+file.open("info.lua", "r")
 status = file.read(1)
 data = file.readline()
 
@@ -23,72 +23,35 @@ datasplit = {}
 
 index = 0
 
+
+
 if (status == "0") or (status=="") or (status==nil) then
-print("Wrong")
-dofile("GetNetwork.lua")
-print("INTER")
-else
-
-for i in string.gmatch(data, '([^,]+)') do
-  datasplit[index] = i
-  index = index + 1
-end
-file.close()
-
--- # real program
-
-print(datasplit[2])
-print(datasplit[4])
-wifi.setmode(wifi.STATION)
-wifi.sta.config(datasplit[2],datasplit[4])
-print(wifi.sta.getip())
-
-
-
-end
-pin = 4
-
-gpio.mode(pin,gpio.INT)
-function Connect(level)
+	print("Wrong")
 	dofile("GetNetwork.lua")
 	print("INTER")
-	if level == 1 then gpio.trig(1, "down") else gpio.trig(1, "up") end
-end
-
-gpio.trig(pin, "up",Connect)
-
-print(tmr.now())
-  
-file.open("settings.lua", "r")
-if (file.open("settings.lua", "r") == nil) then
-	print("No file")
-	dofile("GetNetwork.lua")
-end
-status = file.read(1)
-data = file.readline()
-
-datasplit = {}
-
-index = 0
-
-if (status == "0") or (status=="") or (status==nil) then
-print("Wrong")
-dofile("GetNetwork.lua")
-print("INTER")
 else
 
 for i in string.gmatch(data, '([^,]+)') do
   datasplit[index] = i
   index = index + 1
 end
+
 file.close()
 
--- # real program
+print("starting temp program")
 
-print(datasplit[2])
-print(datasplit[4])
 wifi.setmode(wifi.STATION)
 wifi.sta.config(datasplit[2],datasplit[4])
-print(wifi.sta.getip())
+wifi.sta.connect()
+tmr.alarm(1, 10, 1, function() 
+if not (wifi.sta.getip() == nil) then 
+tmr.stop(1)
+--print("Config done, IP is "..wifi.sta.getip())
 
-end
+--Real Program
+
+
+end 
+end)
+end 
+
